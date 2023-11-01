62149a622435ab795a821b2746296b1e7377c91b
evan <evan@vers.kr>
2023-11-01 15:41:40 +0900

    [remove] server 파일 삭제

--- server/Backend_ttaleum/src/main/java/com/example/ttaleum/Product/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ttaleum.Product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<ProductEntity, Long>{
-
-}
