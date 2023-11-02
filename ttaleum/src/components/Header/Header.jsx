81c50265fbe0baea8fe17a42c7a290b7128733c8
evan <evan@vers.kr>
2023-11-02 17:02:34 +0900

    [style] Link -> Router로 변경, navigation layout으로 이동

--- ttaleum/src/components/Header/Header.jsx
+++ ttaleum/src/components/Header/Header.jsx
@@ -3,14 +3,17 @@
 import './Header.scss';
 
 import Image from "next/image";
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
 import Logo from "../../../public/Logo.svg";
 import search from '../../../public/search.svg';
 import heart from '../../../public/heart.svg';
 import cart from '../../../public/cart.svg';
-import back from '../../../public/backBtn.svg'
+import back from '../../../public/backBtn.svg';
 
 export default function Header({ title }) {
+    const router = useRouter();
+
     return (
         <>
             {title ? (
@@ -19,20 +22,16 @@ export default function Header({ title }) {
                     <div className="header-menu">
                         <Image src={heart} alt='heart' />
                         <Image src={cart} alt='cart' />
-                        <Image onClick={()=>{window.history.back()}} src={back} alt='back' />
+                        <Image onClick={() => { window.history.back() }} src={back} alt='back' />
                     </div>
                 </div>
             ) : (
                 <div className="header-container">
-                    <Link href={'/'}>
-                        <Image src={Logo} alt="Logo" />
-                    </Link>
+                    <Image onClick={() => { router.push('/') }} src={Logo} alt="Logo" />
                     <div className="header-menu">
-                        <Link href={'/search'}>
-                            <Image src={search} alt='search' />
-                        </Link>
-                        <Image src={heart} alt='heart' />
-                        <Image src={cart} alt='cart' />
+                        <Image onClick={() => { router.push('/search') }} src={search} alt='search' />
+                        <Image onClick={() => { router.push('/') }} src={heart} alt='heart' />
+                        <Image onClick={() => { router.push('/') }} src={cart} alt='cart' />
                     </div>
                 </div>
             )}
